f8dd4a188b289ab9ba28a4a09a2271c1d3cd1bd7
Thien at BPS <132377951+Thien-BPS@users.noreply.github.com>
2023-05-09 13:21:07 +0000

    I tried out rails.

---
+++ csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you want to say? ");
+            string? getInput = Console.ReadLine();
+            Console.WriteLine(getInput);
+        }
+    }
+}
