6c7a4d260034614e61ffb80fafe58d741086787f
Thien at BPS <132377951+Thien-BPS@users.noreply.github.com>
2024-06-14 13:31:47 +0000

    no

--- csharp/obj/Debug/net7.0/csharp.AssemblyInfo.cs
+++ csharp/obj/Debug/net7.0/csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4751f040d51b727b6323081c218dd272ee8be1c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
